805b3a598c01136027e9e3edf305e4f31e186a6c
Meliodas Br <meliodas.br.oficial@gmail.com>
2024-07-09 23:40:56 -0300

    Add files via upload

--- Simulado.js
+++ Simulado.js
@@ -11,7 +11,7 @@ const questions = [
     },
     {
         question: "Quem pintou a Mona Lisa?",
-        image: null,
+        image: "/monalisa.jpg",
         answers: [
             { text: "Leonardo da Vinci", correct: true },
             { text: "Vincent van Gogh", correct: false },
@@ -60,7 +60,7 @@ const questions = [
         ]
     },
     {
-        question: "Leia o texto abaixo para responder à questão.\n\nA agricultura urbana é uma prática crescente em diversas cidades ao redor do mundo. Além de contribuir para a segurança alimentar local, ela desempenha um papel crucial na sustentabilidade urbana, promovendo a redução da pegada de carbono e o uso eficiente dos recursos naturais disponíveis nas áreas urbanas. Um dos desafios enfrentados por agricultores urbanos é a qualidade do solo, que muitas vezes é prejudicada pela contaminação e pela falta de nutrientes essenciais. Para mitigar esses problemas, são adotadas técnicas como o uso de compostagem, que transforma resíduos orgânicos em adubo rico em nutrientes para o solo, e a rotação de culturas, que ajuda a manter a fertilidade do solo.\n\nConsiderando as informações do texto, é correto afirmar que a agricultura urbana:\n\nA) Aumenta a pegada de carbono nas cidades.\nB) Não contribui para a segurança alimentar local.\nC) Não enfrenta desafios relacionados à qualidade do solo.\nD) Promove a sustentabilidade urbana.\nE) Reduz a eficiência no uso de recursos naturais.",
+        question: "Leia o texto abaixo para responder à questão.\n\nA agricultura urbana é uma prática crescente em diversas cidades ao redor do mundo. Além de contribuir para a segurança alimentar local, ela desempenha um papel crucial na sustentabilidade urbana, promovendo a redução da pegada de carbono e o uso eficiente dos recursos naturais disponíveis nas áreas urbanas. Um dos desafios enfrentados por agricultores urbanos é a qualidade do solo, que muitas vezes é prejudicada pela contaminação e pela falta de nutrientes essenciais. Para mitigar esses problemas, são adotadas técnicas como o uso de compostagem, que transforma resíduos orgânicos em adubo rico em nutrientes para o solo, e a rotação de culturas, que ajuda a manter a fertilidade do solo.\n\nConsiderando as informações do texto, é correto afirmar que a agricultura urbana:",
         image: null,
         answers: [
             { text: "Aumenta a pegada de carbono nas cidades.", correct: false },
@@ -71,7 +71,7 @@ const questions = [
         ]
     },
     {
-        question: "Leia o trecho abaixo para responder à questão.\n\nNos últimos anos, tem havido um aumento significativo no número de startups voltadas para a economia circular, um modelo econômico que visa maximizar o uso de recursos existentes e minimizar o desperdício. Essas startups não apenas desenvolvem produtos inovadores, mas também reimaginam processos industriais tradicionais para reduzir seu impacto ambiental. Um exemplo disso é a reciclagem de materiais que antes eram considerados resíduos, transformando-os em novos produtos de valor agregado. Além disso, a economia circular fomenta a colaboração entre empresas de diferentes setores, promovendo um ciclo contínuo de inovação e sustentabilidade.\n\nCom base no texto, pode-se inferir que a economia circular:\n\nA) Prioriza o aumento do desperdício de recursos.\nB) Ignora a necessidade de colaboração entre empresas.\nC) Fomenta a inovação e a sustentabilidade.\nD) Reduz a importância de produtos inovadores.\nE) Aumenta o impacto ambiental dos processos industriais.",
+        question: "Leia o trecho abaixo para responder à questão.\n\nNos últimos anos, tem havido um aumento significativo no número de startups voltadas para a economia circular, um modelo econômico que visa maximizar o uso de recursos existentes e minimizar o desperdício. Essas startups não apenas desenvolvem produtos inovadores, mas também reimaginam processos industriais tradicionais para reduzir seu impacto ambiental. Um exemplo disso é a reciclagem de materiais que antes eram considerados resíduos, transformando-os em novos produtos de valor agregado. Além disso, a economia circular fomenta a colaboração entre empresas de diferentes setores, promovendo um ciclo contínuo de inovação e sustentabilidade.\n\nCom base no texto, pode-se inferir que a economia circular:",
         image: null,
         answers: [
             { text: "Prioriza o aumento do desperdício de recursos.", correct: false },
@@ -82,7 +82,7 @@ const questions = [
         ]
     },
     {
-        question: "Leia o texto abaixo para responder à questão.\n\nA nanotecnologia é um campo da ciência que estuda materiais e dispositivos extremamente pequenos, na escala de nanômetros (um bilionésimo de metro). Essa tecnologia tem aplicações em diversas áreas, desde a medicina até a eletrônica, devido às propriedades únicas dos materiais nanométricos. Um dos desafios enfrentados pelos pesquisadores é garantir a segurança desses materiais, uma vez que seu comportamento pode ser diferente dos materiais convencionais em escala macroscópica. Portanto, são necessários estudos rigorosos para avaliar os potenciais impactos ambientais e de saúde associados ao uso da nanotecnologia.\n\nCom base no texto, pode-se afirmar que um dos desafios da nanotecnologia é:\n\nA) A falta de aplicações práticas.\nB) A manipulação de materiais em grande escala.\nC) A ausência de propriedades únicas dos materiais nanométricos.\nD) A garantia da segurança dos materiais.\nE) A resistência dos materiais à eletrônica convencional.",
+        question: "Leia o texto abaixo para responder à questão.\n\nA nanotecnologia é um campo da ciência que estuda materiais e dispositivos extremamente pequenos, na escala de nanômetros (um bilionésimo de metro). Essa tecnologia tem aplicações em diversas áreas, desde a medicina até a eletrônica, devido às propriedades únicas dos materiais nanométricos. Um dos desafios enfrentados pelos pesquisadores é garantir a segurança desses materiais, uma vez que seu comportamento pode ser diferente dos materiais convencionais em escala macroscópica. Portanto, são necessários estudos rigorosos para avaliar os potenciais impactos ambientais e de saúde associados ao uso da nanotecnologia.\n\nCom base no texto, pode-se afirmar que um dos desafios da nanotecnologia é:",
         image: null,
         answers: [
             { text: "A falta de aplicações práticas.", correct: false },
@@ -134,7 +134,7 @@ const questions = [
     },
     {
         question: "Quem pintou 'A Noite Estrelada'?",
-        image: null,
+        image: "/A noite Estrelada.jpg",
         answers: [
             { text: "Vincent van Gogh", correct: true },
             { text: "Pablo Picasso", correct: false },
@@ -271,8 +271,138 @@ const questions = [
             { text: "Fortalecer a monarquia", correct: false },
             { text: "Promover a industrialização", correct: false }
         ]
+    },
+    {
+        question: "A imagem acima faz referência a momentos de tensão na política externa Brasileira durante o século XIX(19). Um desses episódios envolveu a Inglaterra na chamada Questão Christie (1863) e foi deflagrada pela",
+        image: "/Questao-de-historia.jpg",
+        answers: [
+            { text: "resistência das elites escravistas brasileiras em extinguir o tráfico de africanos, gerando descontentamento entre os diplomatas ingleses.", correct: false },
+            { text: "decisão do governo brasileiro de não renovar o tratado de comércio com a Inglaterra, favorecendo a situação financeira do governo imperial.", correct: false },
+            { text: "aprovação da lei Bill Aberdeen pelo Parlamento inglês, proibindo o tráfico de escravos no Atlântico, sob pena de apreensão de navios negreiros.", correct: false },
+            { text: "pilhagem da carga de um navio inglês naufragado no Brasil e pelo aprisionamento, pela Inglaterra, de navios brasileiros no Rio de Janeiro.", correct: true }
+        ]
+    },
+    {
+        question: "A Lei de Terras, aprovada em 1850, duas semanas após a proibição do tráfico de escravos, tentou pôr ordem na confusão existente em matéria de propriedade rural, determinando que, no futuro, as terras públicas fossem vendidas e não doadas, como acontecera com as antigas sesmarias, estabeleceu normas para legalizar a posse de terras e procurou forçar o registro das propriedades.\n\nSobre essa Lei de Terras é correto afirmar que",
+        image: null,
+        answers: [
+            { text: "Sua promulgação coincidiu com a Lei Eusébio de Queiroz, mas não há nenhuma relação de causalidade entre ambas.", correct: false },
+            { text: "Ao entrar em vigor, não foi respeitada, podendo ser considerada mais uma ''lei para inglês ver''.", correct: false },
+            { text: "Sua promulgação foi concebida como uma forma de evitar o acesso à propriedade da terra por parte de futuros imigrantes.", correct: true },
+            { text: "Sua aprovação naquele momento decorreu de os Estados Unidos terem acabado de aprovar uma lei de terras para o seu território.", correct: false }
+        ]
+    },
+    {
+        question: "O ano de 1848 assistiu a várias revoluções na Europa como, por exemplo, na França e na Itália. O espírito ''quarenta e oito'', como se chamou este período, também atingiu o Brasil e, particularmente, Pernambuco. Esta questão diz respeito à Revolução Praieira.",
+        image: null,
+        answers: [
+            { text: "Os revolucionários praieiros pretendiam que o Governo interviesse nos fenômenos de produção, distribuição e comércio promovendo nosso retorno à condição de colônia.", correct: false },
+            { text: "Os revolucionários de Pernambuco lançaram um ''Manifesto ao Mundo'' esclarecendo suas posições no que diz respeito ao voto universal do povo brasileiro, ao trabalho como garantia de vida para o cidadão brasileiro e à reforma do poder judicial dentre outras.", correct: true },
+            { text: "O Partido da Praia, integrado por conservadores pernambucanos, tinha no jornal o Diário Novo um instrumento de veiculação de suas ideias políticas.", correct: false },
+            { text: "A concentração da propriedade fundiária e a permanência da gestão holandesa em Pernambuco foram fatores que provocaram a Revolução Praieira.", correct: false }
+        ]
+    },
+    {
+        question: "Na segunda metade do século XIX, o governo brasileiro realizou uma série de iniciativas, no que diz respeito ao desenvolvimento urbano. Sobre esta questão, assinale a alternativa correta.",
+        image: null,
+        answers: [
+            { text: "Grandes empreendimentos fluviais surgiram, fundamentados na vasta rede hidrográfica que o país possui - o Rio São Francisco e o Rio Araguaia, são exemplos de navegabilidade sem dificuldades.", correct: false },
+            { text: "Construções de represas, aproveitando o potencial hidrográfico dos rios São Francisco, Amazonas e Paraná para a produção de energia, facilitaram a industrialização.", correct: false },
+            { text: "A navegação marítima, o transporte terrestre, incluindo as ferrovias, a iluminação a gás e o abastecimento d água foram algumas iniciativas que mudaram a face das grandes cidades do país.", correct: true },
+            { text: "O desenvolvimento de uma malha ferroviária, não apenas para escoar a produção agrícola, mas para ligar regiões, possibilitou o crescimento industrial em regiões interioranas.", correct: false }
+        ]
+    },
+    {
+        question: "O descontentamento do Exército, que culminou na Questão Militar no final do Império, pode ser atribuído",
+        image: null,
+        answers: [
+            { text: "à predominância do poder civil que não prestigiava os militares e lhes proibia o debate político pela imprensa.", correct: true },
+            { text: "às pressões exercidas pela Igreja junto aos militares para abolir a monarquia.", correct: false },
+            { text: "à propaganda do militarismo sul-americano na imprensa brasileira.", correct: false },
+            { text: "às tendências ultrademocráticas das forças armadas, que desejavam conceder maior participação política aos analfabetos.", correct: false }
+        ]
+    },
+    {
+        question: "A chamada ''Questão Religiosa'', ocorrida durante o Segundo Reinado, foi uma disputa entre o Estado Imperial e a Igreja Católica devido à",
+        image: null,
+        answers: [
+            { text: "rejeição, pelo governo, dos dispositivos da bula Syllabus, baixada pelo papa Pio IX, que proibia a permanência de membros da maçonaria dentro dos quadros da Igreja.", correct: true },
+            { text: "rejeição da encíclica Rerum Novarum, baixada pelo papa Leão XIII, que defendia a coexistência harmoniosa do capital e do trabalho, no sentido de evitar a luta de classes.", correct: false },
+            { text: "adesão do governo imperial aos ditames do Tratado de Latrão, que limitava os poderes da Igreja expressos na instituição do Padroado.", correct: false },
+            { text: "recusa do governo de Dom Pedro II em aceitar as manobras parlamentares dos deputados católicos, visando à extinção do direito do Padroado.", correct: false }
+        ]
+    },
+    {
+        question: "A estabilidade do Império brasileiro após 1850 favoreceu o crescimento e a prosperidade econômica. Nesse período, os empreendimentos econômicos mais arrojados partiram do barão de Mauá, que construiu ferrovias, criou bancos, investiu na navegação fluvial. Contudo, as iniciativas de Mauá na área industrial encontraram fortes obstáculos, a saber:",
+        image: null,
+        answers: [
+            { text: "a carência de mão de obra assalariada e a concorrência das manufaturas inglesas, favorecidas pelas taxas de importação.", correct: true },
+            { text: "a expansão da cafeicultura no Oeste paulista e o início da organização do movimento operário.", correct: false },
+            { text: "a tarifa alfandegária benéfica à produção nacional e o declínio da economia açucareira da região Nordeste.", correct: false },
+            { text: "a concorrência com a emergente produção industrial do Paraguai.", correct: false }
+        ]
+    },
+    {
+        question: "Em qual das frases a seguir a crase está empregada corretamente?",
+        image: null,
+        answers: [
+            { text: "A reunião será à partir das 14h.", correct: false },
+            { text: "Vou à escola todos os dias.", correct: true },
+            { text: "Ela entregou o trabalho à tempo.", correct: false },
+            { text: "Fomos à praia no final de semana passada.", correct: false }
+        ]
+    },
+    {
+        question: "Qual das opções apresenta um exemplo correto de voz passiva analítica?",
+        image: null,
+        answers: [
+            { text: "As provas foram corrigidas pelo professor.", correct: true },
+            { text: "O professor corrigiu as provas.", correct: false },
+            { text: "Corrigiram as provas.", correct: false },
+            { text: "As provas estão corrigindo.", correct: false }
+        ]
+    },
+    {
+        question: "Qual das frases a seguir apresenta um exemplo de regência nominal correta?",
+        image: null,
+        answers: [
+            { text: "Ela é fiel a seus princípios.", correct: true },
+            { text: "Ele é ansioso com a viagem.", correct: false },
+            { text: "A professora é rigorosa em os alunos.", correct: false },
+            { text: "João está atento com os detalhes.", correct: false }
+        ]
+    },
+    {
+        question: "Qual das opções contém um exemplo correto de concordância verbal?",
+        image: null,
+        answers: [
+            { text: "Fazem dois anos que não nos vemos.", correct: true },
+            { text: "A maioria das pessoas concordaram com a decisão.", correct: false },
+            { text: "Houveram muitos problemas na execução do projeto.", correct: false },
+            { text: "Fui eu que fez o trabalho.", correct: false }
+        ]
+    },
+    {
+        question: "Em qual das opções a vírgula está corretamente empregada?",
+        image: null,
+        answers: [
+            { text: "Se você for, eu também vou.", correct: true },
+            { text: "Ela gosta de ler, e de escrever.", correct: false },
+            { text: "O filme, que assistimos ontem foi emocionante.", correct: false },
+            { text: "Pedro foi ao mercado comprar frutas e verduras, para o jantar.", correct: false }
+        ]
+    },
+    {
+        question: "Qual das frases apresenta a correta flexão do verbo ''intervir'' no pretérito perfeito do indicativo?",
+        image: null,
+        answers: [
+            { text: "Ele interviu na reunião.", correct: false },
+            { text: "Nós interviemos no conflito.", correct: false },
+            { text: "Ela interveio na discussão.", correct: true },
+            { text: "Eu intervi a situação.", correct: false }
+        ]
     }
-    // Adicione mais perguntas conforme necessário
+
 ];
 
 let selectedQuestions = [];
