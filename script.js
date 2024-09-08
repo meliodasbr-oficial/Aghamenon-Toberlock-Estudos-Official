ed97cfe5709ecd321bc0d20f42f45876167f3520
Meliodas Br <meliodas.br.oficial@gmail.com>
2024-09-08 00:58:53 -0300

    1.0.2

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Substitui o conteúdo da tag <body> com o conteúdo de manutenção
-    document.body.innerHTML = `
-        <div class="container">
+    // Cria uma div que atuará como o "body secundário"
+    const bodySecundario = document.createElement('div');
+    bodySecundario.classList.add('body-secundario');
+    
+    // Define o conteúdo do "body secundário"
+    bodySecundario.innerHTML = `
+        <div class="manutencao">
             <h1>Avisos da Manutenção</h1>
-            <div id="avisos-container"></div>
+            <div id="avisos-manutencao"></div>
         </div>
     `;
 
-    const avisosContainer = document.getElementById('avisos-container');
+    // Substitui o conteúdo do body principal pelo "body secundário"
+    document.body.innerHTML = '';
+    document.body.appendChild(bodySecundario);
+
+    const avisosContainer = document.getElementById('avisos-manutencao');
 
     const avisosDoServidor = [
         { titulo: 'Aghamenon Toberlock Estudos', mensagem: '' },
         { titulo: 'Em Manutenção', mensagem: 'Site estará ativo dia 09/09/2024 às 08:00h (horário de Brasília).' },
-        { titulo: 'Detalhes da Manutenção', mensagem: 'Melhoração de Interface e Otimização do Site.\nNovas Funcionalidades\nNovos Simulados\nSistema de Login\nSistema de Ranking' }
+        { titulo: 'Detalhes da Manutenção', mensagem: 'Melhoria de Interface e Otimização do Site.\nNovas Funcionalidades\nNovos Simulados\nSistema de Login\nSistema de Ranking' }
     ];
 
     // Função para exibir os avisos na página
